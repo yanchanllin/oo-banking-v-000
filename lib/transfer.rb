071737157d2ac413d153dd9867cdd59e087c1f02
YanC Lin <yonalin1@gmail.com>
2017-11-17 20:28:33 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -31,11 +31,11 @@ class Transfer
     end
 
     def reverse_transfer
-      if valid? && @status == "complete"
-        @sender.deposit(@amount)
-        @reciever.deposit(@amount *-1)
-        @status = "reversed"
-      end
+      def reverse_transfer
+    if @status == "complete"
+      @sender.deposit( @amount ) 
+      @receiver.deposit( @amount * -1)
+      @status = "reversed"
     end
-
+  end
 end
