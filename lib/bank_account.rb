0be1b0953ce9c12802a76ec6958668a49800963d
YanC Lin <yonalin1@gmail.com>
2017-11-17 19:30:01 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,8 @@
 class BankAccount
-  attr_accessor :name
+  attr_accessor :name,:balance
 
-
-    def initialize(name)
+    def initialize(name,balance=1000)
         @name = name
-
+        @balance = balance
       end
 end
