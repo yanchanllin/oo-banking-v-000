f387eaab363a369d1a2f9204a36327301889ee56
YanC Lin <yonalin1@gmail.com>
2017-11-17 20:14:44 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,11 @@ class Transfer
 
   def execute_transaction
     if @sender.balance < @amount
+      @status = "rejected"
       return "Transaction rejected. Please check your account balance."
+
+    elsif @status == "complete"
+
     else
       @sender.deposit( @amount * -1 )
       @receiver.deposit( @amount )
