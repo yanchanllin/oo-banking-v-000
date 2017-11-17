cb364eb56420c591833dc59a14821b30f7e03723
YanC Lin <yonalin1@gmail.com>
2017-11-17 19:33:38 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,15 @@
 class BankAccount
   attr_accessor :balance,:status
   attr_reader :name
+  
     def initialize(name,balance=1000,status="open")
         @name = name
         @balance = balance
         @status = status
       end
+      
+    def deposit
+      self << balance
+    end 
+    
 end
